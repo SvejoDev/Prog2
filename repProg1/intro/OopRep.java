8a10a14a0f5063ec88b062e559383027380d7f62
Johan <johan.svensson@gtsmarketing.se>
2024-08-13 14:13:13 +0200

    intro done

--- repProg1/intro/OopRep.java
+++ repProg1/intro/OopRep.java
@@ -1,6 +1,7 @@
 package intro;
 
 public class OopRep {
+	int tal = 5;
 
 		public OopRep(){
 			int tal = 5;
@@ -18,6 +19,15 @@ public class OopRep {
 		obj[0] = new Inst(text, 4.4, 4.2);
 		System.out.println(obj[0].omkr());
 		// TODO Auto-generated method stub
+		
+		//testa den andra konstruktorn
+		obj[1] = new Inst("Kvadrat", tal);
+		try {
+			for(int i = 0; i < obj.length; i++) {
+				System.out.println(obj[i].getShape());
+			}
+			
+		}catch(Exception e) {};
 		
 	}
 	public static void main(String[] args) {
