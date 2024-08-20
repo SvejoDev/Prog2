e3f1eb15de1a563e80e2197c9459eabee9791118
Johan <johan.svensson@gtsmarketing.se>
2024-08-20 14:27:24 +0200

    game()

---
+++ SpelProg2/spel1/Game.java
@@ -0,0 +1,84 @@
+package spel1;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.HashMap;
+
+import se.egy.graphics.*;
+
+public class Game implements KeyListener{
+    private HashMap<String, Boolean> keyDown = new HashMap<>();
+
+	
+    private ImgContainer player;
+
+
+	private boolean gameRunning = true;
+
+	private GameScreen gameScreen = new GameScreen("Game", 800, 600, false); // false vid testkörning
+
+	public Game(){
+		gameScreen.setKeyListener(this);
+		
+		keyDown.put("left", false);
+		keyDown.put("right", false);
+		
+		loadImages();
+		gameLoop();
+	}
+
+	public void loadImages(){
+		player = new ImgContainer(384, 284, "/playerImg.png");
+
+	}
+
+	public void update(){
+		 if(keyDown.get("right"))
+		        player.setX(player.getX() + 5);
+		    if(keyDown.get("left"))
+			player.setX(player.getX() - 5);
+
+	}
+
+	public void render(){
+	    gameScreen.render(player);
+
+	}
+
+	public void gameLoop(){
+		while(gameRunning){
+			update();
+			render();
+			// Fördröjning
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {}
+		}
+	}
+
+	/** Spelets tangentbordslyssnare */
+	public void keyTyped(KeyEvent e) {
+	}
+
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+
+	     if(key == KeyEvent.VK_LEFT)
+	        keyDown.put("left", true);
+	     else if(key == KeyEvent.VK_RIGHT)
+	        keyDown.put("right", true);
+	}
+
+	public void keyReleased(KeyEvent e) {
+		int key = e.getKeyCode();
+
+	     if(key == KeyEvent.VK_LEFT)
+	        keyDown.put("left", false);
+	     else if(key == KeyEvent.VK_RIGHT)
+	        keyDown.put("right", false);
+	}
+
+	public static void main(String[] args) {
+		new Game();
+	}
+}
