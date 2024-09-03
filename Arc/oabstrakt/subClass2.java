1868c57b298e74264aa5b428140a1ed4e918cb9e
Johan <johan.svensson@gtsmarketing.se>
2024-09-03 13:06:33 +0200

    subClasses

---
+++ Arc/oabstrakt/subClass2.java
@@ -0,0 +1,15 @@
+package oabstrakt;
+
+public class subClass2 extends superClass{
+
+	public subClass2(String text) {
+		super(text);
+		
+	}
+	
+	public void print() {
+		System.out.println(text);
+		
+	}
+
+}
--- Arc/oabstrakt/superClass.java
+++ Arc/oabstrakt/superClass.java
@@ -1,5 +1,39 @@
 package oabstrakt;
 
 public class superClass {
-
+	/// en superklass är en klass som andra klasser ärver.
+	protected int tal = 1;
+	protected String text;
+	private int a;
+	private String txt = "secret";
+	
+	public superClass(String text) {
+		this.text = text;
+	}
+	
+	public int sum() {
+		return tal + a;
+	}
+	
+	public int getA() {
+		return a;
+	}
+	
+	public String getTxt() {
+		return txt;
+	}
+	
+	public void setA(int a) {
+		this.a = a;
+		
+	}
+	
+	public void setTxt(String txt) {
+		this.txt = txt;
+	}
+	
+	public void print() {
+		System.out.println(text);
+	}
 }
+
