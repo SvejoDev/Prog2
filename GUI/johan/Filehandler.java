f29965dd662687de57940b2e6c6cba790bd5b9b7
Johan <johan.svensson@svejo.se>
2024-11-05 13:33:38 +0100

    loadState

--- GUI/johan/Filehandler.java
+++ GUI/johan/Filehandler.java
@@ -1,5 +1,32 @@
 package johan;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class Filehandler {
+private static final String SAVE_FILE = "vending_machine_state";
+
+public static void saveState(VMLogik logic) {
+    try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(SAVE_FILE))) {
+    	oos.writeObject(logic);
+    	System.out.println("Varuautomat status sparad");
+    }catch (IOException e) {
+    	System.err.println("Fel med att spara vauatuomat status" + e.getMessage());
+    }
+}
+public static VMLogik loadState() {
+	try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(SAVE_FILE))){
+		VMLogik logic = (VMLogik) ois.readObject();
+    	System.out.println("Varuatomat data laddat lyckat");
+    	return logic;
+		}catch (IOException | ClassNotFoundException e) {
+		System.err.println("Fel med att ladda varuautomat data" + e.getMessage());
+		return new VMLogik();
+
+		}
+}
 
 }
